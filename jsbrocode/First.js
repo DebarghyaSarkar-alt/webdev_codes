e333065df9a0f117e23911f4705f4a2648614b78
DebarghyaSarkar-alt <sarkardebarghya47@gmail.com>
2026-05-17 20:17:38 +0530

    first

---
+++ jsbrocode/First.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log("Welcome to JavaScript");
+let a = 10;
+let b = "10";
+console.log(a==b);//true
+console.log(a===b);//false  
+let name;
+console.log(name);//undefined
+name = "BroCode";
+console.log(name);//BroCode
